6538c380142d48066aa29c95b7da6e09ea48009a
shivam jain <shivam@vinsol.com>
2017-08-19 19:08:46 +0530

    solutions for exercise 2.1 Selecting

---
+++ exercises/js/sandbox.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  selectElements();
+});
+
+function selectElements() {
+  // div with class module
+  $classModuleDiv = $('div.module');
+
+  // three selectors to get the third item in the #myList
+  $myListItem1 = $('#myListItem');
+  $myListItem2 = $('#myList > li').eq(2);
+  $myListItem3 = $('#myList').children().eq(2);
+  // $myListItem1 is fastest , as it uses ID
+
+  // label for the search input
+  $searchInputLabel = $("label[for='q']");
+
+  // hidden elements on page
+  $hiddenElementsCount = $(':hidden').length;
+
+  // images with alt attribute
+  $imagesWithAlt = $('img[alt]').length;
+
+  // // odd table rows in table body
+  $oddRows = $('tbody > tr:odd');
+}
