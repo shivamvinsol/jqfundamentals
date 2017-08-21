8c03d1fa1541b3902234ea5e0be41e91368164ad
shivam jain <shivam@vinsol.com>
2017-08-21 13:03:19 +0530

    added solution for exercise 2.2 traversal

--- exercises/js/sandbox.js
+++ exercises/js/sandbox.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
   selectElements();
+  traverseElements();
 });
 
 function selectElements() {
@@ -23,4 +24,38 @@ function selectElements() {
 
   // // odd table rows in table body
   $oddRows = $('tbody > tr:odd');
+}
+
+function traverseElements() {
+  // log each image's alt attribute
+  $images = $('img');
+  $images.each(function() {
+    console.log(this.alt);
+  });
+
+  // select search input textbox
+  $searchTextBox = $('input[name="q"]');
+  // go to form, add a class to the form
+  $searchTextBox.parent().addClass('class1');
+
+  // first item in list #mylist with class current
+  $firstListItem = $('#myList > li.current').first();
+  // remove class from it
+  $firstListItem.removeClass('current');
+  // go to next list item, // add class current to it
+  $nextListItem = $firstListItem.next().addClass('current');
+
+  // select inside specials
+  $selectInsideSpecials = $('#specials select[name="day"]');
+  // traverse to submit button
+  $submitButton = $selectInsideSpecials.parent().next().children().first();
+
+  // Select the first list item in the #slideshow element;
+  $firstItemInSlideshow = $('#slideshow > li').first();
+  //  add the class "current" to it
+  $firstItemInSlideshow.addClass('current');
+  // add class of disabled to its sibling elements.
+  $firstItemInSlideshow.siblings().each(function() {
+    $(this).addClass('disabled');
+  });
 }
