509c70a859036db51c37b992464eafd22c11abef
shivam jain <shivam@vinsol.com>
2017-08-25 16:20:51 +0530

    updated blog.js

--- exercises/js/blog.js
+++ exercises/js/blog.js
@@ -1,43 +1,29 @@
-function HiddenTextManager(pageElements) {
-  this.$blog = pageElements.blog;
-  this.$headlines = pageElements.headlines;
-  this.$excerpts = pageElements.excerpts;
+function HiddenTextManager(options) {
+  this.$headlines = options.$headlines;
+  this.$excerpts = options.$excerpts;
 }
+
 HiddenTextManager.prototype.initialize = function() {
   this.createClickEvent();
-}
+};
 
 HiddenTextManager.prototype.createClickEvent = function() {
   var _this = this;
-  this.$headlines.each(function() {
-    _this.bindClickEvent($(this));
-  });
-}
-
-HiddenTextManager.prototype.bindClickEvent = function($headline) {
-  var _this = this;
-  $headline.children('h3').bind('click', function() {
+  this.$headlines.on('click', function() {
     event.preventDefault();
-    // if currently open excerpt is clicked again
-    if ($(this).next().is(':visible')) {
-      // close currently open excerpt
-      $(this).next().slideUp();
-    } else {
-      // slide up any other excerpt
-      _this.$excerpts.not($(this)).slideUp();
-      // slide down current excerpt
-      $(this).next().slideDown();
-    }
+    var $this = $(this);
+    $this.next().slideToggle();
+    // close any other opened excerpt
+    _this.$excerpts.not($this.next()).slideUp();
   });
-}
+};
 
 // --------starts
 $(document).ready(function() {
-  var pageElements = {
-    "blog" : $('#blog'),
-    "headlines" : $('#blog li'),
-    "excerpts" : $('p.excerpt')
+  var options = {
+    $headlines : $('#blog li h3'),
+    $excerpts : $('p.excerpt')
   },
-  hiddenText = new HiddenTextManager(pageElements);
+  hiddenText = new HiddenTextManager(options);
   hiddenText.initialize();
 });
