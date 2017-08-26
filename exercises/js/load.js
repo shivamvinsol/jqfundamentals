3f5461cfe2acc38aa278d93623911e385e8d7bc2
shivam jain <shivam@vinsol.com>
2017-08-26 16:54:10 +0530

    added solution for ex 6.1

---
+++ exercises/js/load.js
@@ -0,0 +1,36 @@
+function BlogContentManager(options) {
+  this.$headlines = options.$headlines;
+}
+
+BlogContentManager.prototype.initialize = function() {
+  this.createTargetDiv(); // where content will be displayed
+  this.bindLoadEvent();
+};
+
+BlogContentManager.prototype.createTargetDiv = function() {
+  this.$headlines.each(function() {
+    var $this = $(this);
+    $content = $('<div>');
+    $this.after($content);
+    $this.data('content', $content); // binds div to respective headline, stores reference in $.fn.data
+  });
+
+};
+
+BlogContentManager.prototype.bindLoadEvent = function() {
+  this.$headlines.on('click', function(event) {
+    var $this = $(this);
+    event.preventDefault();
+    var contentLink = $this.find('a').attr('href'),
+      contentId = contentLink.substring(contentLink.indexOf('#')); // gives contentId to fetch from page
+    $(this).data('content').load('data/blog.html ' + contentId); // displays content on click
+   });
+};
+
+$(document).ready(function() {
+  var options = {
+    $headlines : $('#blog li h3')
+  },
+    blogContent = new BlogContentManager(options);
+  blogContent.initialize();
+});
