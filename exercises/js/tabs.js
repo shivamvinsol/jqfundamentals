aa489c03a63e8e9265a2e306eb604ce975e2782a
shivam jain <shivam@vinsol.com>
2017-09-02 17:46:41 +0530

    updated tabs.js

--- exercises/js/tabs.js
+++ exercises/js/tabs.js
@@ -3,46 +3,39 @@ function TabbedNavigationCreator(options) {
 }
 
 TabbedNavigationCreator.prototype.initialize = function () {
-  // hide all div modules
-  this.$divModules.hide();
-  // create unordered list
-  var $list= this.createUnorderedList();
-  var $listItems = $list.find('li');
-  // create click event on list items
-  this.bindClickEvent($listItems);
-  // show first tab
-  $listItems.first().trigger('click');
+  this.$divModules.hide();  // initially hide all div modules
+  this.createUnorderedList();  // create unordered list
+  this.bindEvents();
+  this.$listItems.first().trigger('click'); // show first tab
 };
 
 TabbedNavigationCreator.prototype.createUnorderedList = function() {
-  var $list = $('<ul>');
-  this.$divModules.first().before($list);
-  this.createListItems($list);
-  return $list;
+  this.$list = $('<ul>');
+  this.$divModules.first().before(this.$list);
+  this.createListItems();
 };
 
 TabbedNavigationCreator.prototype.createListItems = function($list) {
   var _this = this;
   this.$divModules.each(function() {
-    var text = $(this).find('h2').text();
-    var $listItem = $('<li>');
-    $listItem.html(text);
-    $list.append($listItem);
+    var text = $(this).find('h2').text(),
+        $listItem = $('<li>').html(text);
+    _this.$list.append($listItem);
   });
 };
 
-TabbedNavigationCreator.prototype.bindClickEvent = function($listItems) {
+TabbedNavigationCreator.prototype.bindEvents = function() {
+  this.$listItems = this.$list.find('li');
   var _this = this;
-  $listItems.on('click', function() {
-    _this.$divModules.hide();
-    var $this = $(this);
-    // select div by id and show it
-    $('#' + $this.html().toLowerCase()).show();
-    // remove current class from items
-    $listItems.removeClass('current');
-    // add current class to clicked item
-    $this.addClass('current');
-  });
+  this.$listItems.on('click', function() { _this.bindClickEvent(this) });
+};
+
+TabbedNavigationCreator.prototype.bindClickEvent = function(listItem) {
+  var $listItem = $(listItem);
+  this.$divModules.hide();
+  $('#' + $listItem.html().toLowerCase()).show(); // show div module with given id
+  $listItem.addClass('current')
+           .siblings().removeClass('current'); // adds current class to itself, and remove current class from other tabs
 };
 
 // starts------------------------
@@ -50,6 +43,6 @@ $(document).ready(function() {
   var options = {
     $divModules : $('div.module')
   },
-  tabNavigation = new TabbedNavigationCreator(options);
+      tabNavigation = new TabbedNavigationCreator(options);
   tabNavigation.initialize();
 });
