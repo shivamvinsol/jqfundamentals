84bce2b59d04f83a05da306e37c3152f22903bdc
shivam jain <shivam@vinsol.com>
2017-09-02 17:55:01 +0530

    updated navigation.js

--- exercises/js/navigation.js
+++ exercises/js/navigation.js
@@ -2,23 +2,11 @@ function MenuManager(options) {
   this.$menuItems = options.$menuItems;
 }
 
-MenuManager.prototype.initialize = function () {
-  this.createDropDownMenu();
-};
-
-MenuManager.prototype.createDropDownMenu = function() {
-  var _this = this;
+MenuManager.prototype.createDropDownMenu = function () {
   this.$menuItems.hover(
     function() {
-      // show children and add class hover on hover
-      var $this = $(this);
-      $this.find('ul').show();
-      $this.addClass('hover');
-    }, function() {
-      // hide children and remove class hover when hover ends
-      var $this = $(this);
-      $this.find('ul').hide();
-      $this.removeClass('hover');
+      $(this).toggleClass('hover')  // adds class 'hover' on hover
+             .find('ul').toggle(); // hover shows sub-items, if any
     }
   );
 };
@@ -28,6 +16,6 @@ $(document).ready(function() {
   var options = {
     $menuItems : $('#nav > li')
   },
-  menu = new MenuManager(options);
-  menu.initialize();
+      menu = new MenuManager(options);
+  menu.createDropDownMenu();
 });
